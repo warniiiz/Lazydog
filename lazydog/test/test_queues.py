d11124753b9c52b0eb9c7a422caf7b7f96a3b8e3
Clément Warneys <clement.warneys@gmail.com>
2018-04-04 17:34:27 +0200

    Adding py.test for queues module

---
+++ lazydog/test/test_queues.py
@@ -0,0 +1,55 @@
+
+import sys
+import os
+import time
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from queues import DatedlocaleventQueue
+from states import LocalState
+from revised_watchdog.observers.inotify import InotifyObserver
+
+TEST_DIR = None
+TESTED_QUEUE = None
+
+def test_instanciate_tested_queue(tmpdir):
+    # tmpdir creates a temporary test dir
+    global TEST_DIR
+    TEST_DIR = str(tmpdir)
+    # create local state
+    local_states = LocalState(str(tmpdir))
+    # create tested queue
+    global TESTED_QUEUE
+    TESTED_QUEUE = DatedlocaleventQueue(local_states)
+
+def test_DatedlocaleventQueue():
+    
+    # Basics
+    assert TESTED_QUEUE.is_empty() 
+    assert TESTED_QUEUE.size() == 0
+    assert TESTED_QUEUE.next() is None
+
+    # Adding an observer using this queue
+    observer = InotifyObserver() # generate_full_events=False) # With reviewed Inotify 
+    observer.schedule(TESTED_QUEUE, TEST_DIR, recursive=True)
+    observer.name = 'Local Inotify observer'
+    observer.start()
+
+    # Generating a created event
+    os.system('touch ' + TEST_DIR + '/test.txt')
+
+    # Checking result - 3 new events during the tests
+    time.sleep(1)
+    assert TESTED_QUEUE.size() > 0
+    assert not TESTED_QUEUE.is_empty() 
+
+    # Getting the events
+    while not TESTED_QUEUE.is_empty():
+        assert TESTED_QUEUE.next().__class__.__name__ == 'LazydogEvent'
+
+    # Checking emptyness of the queue
+    assert TESTED_QUEUE.is_empty() 
+    
+
+
+
+
+
